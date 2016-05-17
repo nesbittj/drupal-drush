f9fe66334cefa15c0d3aabeda5f0372ac5d7d592
jonathan <jonathan@olamalu.com>
2016-05-17 09:22:01 +0100

    bash -c exec style

--- olamalu_run.sh
+++ olamalu_run.sh
@@ -8,26 +8,31 @@ docker build -t olamalujonathan/drupal-drush https://github.com/nesbittj/drupal-
 mysql_root_pwd='rootpwd'
 
 mysql_container_id=$(docker run -d -e MYSQL_ROOT_PASSWORD=$mysql_root_pwd -e MYSQL_USER=drupal -e MYSQL_PASSWORD=drupal mysql:latest)
-echo 'mysql_container_id: '$mysql_container_id
+#echo 'mysql_container_id: '$mysql_container_id
+mysql_container_name=$(docker ps -f id=$mysql_container_id --format "{{.Names}}")
 
 #run container from image, link to db
 drupal_container_id=$(docker run -d --link $mysql_container_id -P olamalujonathan/drupal-drush)
-echo 'drupal_container_id: '$drupal_container_id
+#echo 'drupal_container_id: '$drupal_container_id
 drupal_container_port=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $drupal_container_id)
-echo 'drupal_container_port: '$drupal_container_port
+#echo 'drupal_container_port: '$drupal_container_port
 drupal_container_hostname=$(docker inspect --format='{{.Config.Hostname}}' $mysql_container_id)
-echo 'drupal_container_hostname: '$drupal_container_hostname
+#echo 'drupal_container_hostname: '$drupal_container_hostname
+drupal_container_name=$(docker ps -f id=$drupal_container_id --format "{{.Names}}")
 
 #exec drush site-install on site in container
 #drush si --site-name=sitename --db-url=mysql://root:rootpwd@clever_booth/dbname --db-su=root --db-su-pw=rootpwd -y
-mysql_db_url="mysql://root:$mysql_root_pwd@$mysql_container_id/dbname"
+mysql_db_url="mysql://root:$mysql_root_pwd@$mysql_container_name/$drupal_container_name"
 echo 'mysql_db_url: '$mysql_db_url
 #drush_si_cmd="docker exec $drupal_container_id drush si --site-name=$drupal_container_hostname --db-url=$mysql_db_url --db-su=root --db-su-pw=$mysql_root_pwd -y"
 #drush_si_cmd="docker exec $drupal_container_id drush si --site-name=sitename --db-url=$mysql_db_url --db-su=root --db-su-pw=$mysql_root_pwd -y"
-drush_si_cmd="drush si --site-name=sitename --db-url=$mysql_db_url --db-su=root --db-su-pw=dbname -y"
+drush_si_cmd="drush si --site-name=$drupal_container_name --db-url=$mysql_db_url --db-su=root --db-su-pw=$mysql_root_pwd -y -v --debug"
+#echo $drush_si_cmd > drush_si_cmd.sh
 #eval $drush_si_cmd
 #docker exec -ti $drupal_container_id drush si --site-name=sitename --db-url=$mysql_db_url --db-su='root' --db-su-pw="$mysql_root_pwd" -y -v
-docker exec -ti $drupal_container_id $drush_si_cmd
+#docker exec -ti $drupal_container_id $drush_si_cmd
+docker exec $drupal_container_id bash -c "echo $drush_si_cmd > olamalu_si.sh && chmod +x olamalu_si.sh"
+docker exec $drupal_container_id /bin/bash ./olamalu_si.sh
 
 #exec correct permissions on files dir
 docker exec $drupal_container_id chown -R www-data:www-data sites/default/files
